0b3762c0376b151e4f83aa366f3902ec1a43ed46
zhongchengyi <woshizcy@sina.com>
2020-03-14 18:59:35 +0800

    增加动画

--- assets/scripts/Player.js
+++ assets/scripts/Player.js
@@ -12,32 +12,53 @@ cc.Class({
     extends: cc.Component,
 
     properties: {
+        nameLabel: {
+            type: cc.Label,
+            default: null
+        },
         playerName: {
             default: "玩家名",
             tooltip: '玩家名字',
             notice() {
-                if (this._nameLabel) {
-                    this._nameLabel.text = this.playerName;
+                if (this.nameLabel) {
+                    this.nameLabel.string = this.playerName;
                 }
             }
         },
+        thinking: {
+            default: false,
+            tooltip: '思考中',
+        }
     },
 
     // LIFE-CYCLE CALLBACKS:
 
     onLoad() {
-        this._nameLabel = this.node.getComponent(cc.Label);
-        if (this._nameLabel) {
-            this._nameLabel.text = this.playerName;
+        if (this.nameLabel) {
+            this.nameLabel.string = this.playerName;
+            this.nameLabel.color = this.node.color;
         }
         this.node.on(cc.Node.EventType.MOUSE_DOWN, e => {
             this.node.off(cc.Node.EventType.MOUSE_DOWN);
             this.node.emit('user-decision', {player: this})
         });
+
+
+        this.nameLabel.node.runAction(cc.sequence(
+            cc.tintTo(2, 255, 0, 0),
+            cc.delayTime(0.5),
+            cc.fadeOut(0.5),
+            cc.delayTime(0.5),
+            cc.fadeIn(0.5),
+            cc.delayTime(0.5),
+            cc.tintTo(2, 255, 255, 255)
+        ).repeat(2));
     },
 
     start() {
     },
 
-    // update (dt) {},
+    update(dt) {
+
+    },
 });
